ac69361b03f4600fade0a86051aafb8a660ccf66
mirmak4 <miron.maksymiuk@globallogic.com>
2022-04-01 18:07:03 +0200

    final commit by American guy

---
+++ src/test/java/pl/kazanik/maven_java/DemoTest.java
@@ -0,0 +1,11 @@
+package pl.kazanik.maven_java;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+    @Test
+    public void demoTest() {
+        System.out.println("demo test case");
+    }
+}
