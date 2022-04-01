a0b685d3d2fcd5986ae2d677b3d0c3279deea969
mirmak4 <miron.maksymiuk@globallogic.com>
2022-04-01 13:23:46 +0200

    this is commit by American guy

--- src/main/java/pl/kazanik/maven_java/App.java
+++ src/main/java/pl/kazanik/maven_java/App.java
@@ -11,5 +11,7 @@ public class App
 
         System.out.println( "Hello World!" );
         System.out.println("This is GIT X workspace");
+        System.out.println("Now is work by American guy");
+        System.out.println("OK its done");
     }
 }
